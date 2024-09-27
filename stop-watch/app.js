2741a2d90be1f1fd479f46ac0d18b08f94de44b6
anwarnagori <ar1498137@gmail.com>
2024-09-27 07:03:17 -0700

    complete functions

--- stop-watch/app.js
+++ stop-watch/app.js
@@ -1,44 +1,44 @@
-let min = 0;
-let sec = 0;
-let msec = 0;
+// let min = 0;
+// let sec = 0;
+// let msec = 0;
 
-let htmlmin = document.getElementById("min");
-let htmlsec = document.getElementById("sec");
-let htmlmsec = document.getElementById("msec");
+// let htmlmin = document.getElementById("min");
+// let htmlsec = document.getElementById("sec");
+// let htmlmsec = document.getElementById("msec");
 
-let interval;
+// let interval;
 
-let disabledBtn = document.getElementById("start");
+// let disabledBtn = document.getElementById("start");
 
-function start() {
-    interval = setInterval(() => {
-        msec++;
-        htmlmsec.innerHTML = msec;
+// function start() {
+//     interval = setInterval(() => {
+//         msec++;
+//         htmlmsec.innerHTML = msec;
 
-        if (msec >= 100) {
-            sec++;
-            htmlsec.innerHTML = sec;
-            msec = 0;
-        } else if (sec >= 60) {
-            min++;
-            htmlmin.innerHTML = min;
-            sec = 0;
-        }
-        disabledBtn.disabled = true;
-    }, 0);
-}
+//         if (msec >= 100) {
+//             sec++;
+//             htmlsec.innerHTML = sec;
+//             msec = 0;
+//         } else if (sec >= 60) {
+//             min++;
+//             htmlmin.innerHTML = min;
+//             sec = 0;
+//         }
+//         disabledBtn.disabled = true;
+//     }, 0);
+// }
 
-function pause() {
-    clearInterval(interval);
-    disabledBtn.disabled = false;
-}
+// function pause() {
+//     clearInterval(interval);
+//     disabledBtn.disabled = false;
+// }
 
-function reset() {
-    let min = 0;
-    let sec = 0;
-    let msec = 0;
+// function reset() {
+//     let min = 0;
+//     let sec = 0;
+//     let msec = 0;
 
-    htmlmin.innerHTML = min;
-    htmlsec.innerHTML = sec;
-    htmlmsec.innerHTML = msec;
-}
+//     htmlmin.innerHTML = min;
+//     htmlsec.innerHTML = sec;
+//     htmlmsec.innerHTML = msec;
+// }
